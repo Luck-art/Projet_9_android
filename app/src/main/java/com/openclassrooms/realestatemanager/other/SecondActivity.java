d8b11df1b144fef47ed5bf7981253fe5c848c87e
Lucasdev <potronlucas9@outlook.fr>
2023-09-15 21:05:26 +0200

    WIP Add drawer_menu on project

--- app/src/main/java/com/openclassrooms/realestatemanager/other/SecondActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/other/SecondActivity.java
@@ -1,8 +1,10 @@
-package com.openclassrooms.realestatemanager;
+package com.openclassrooms.realestatemanager.other;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.openclassrooms.realestatemanager.R;
 
 public class SecondActivity extends AppCompatActivity {
 
