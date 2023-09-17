4fa89c158683c7430d57859226556f6a375cde3d
Lucasdev <potronlucas9@outlook.fr>
2023-09-17 16:30:27 +0200

    Add generator files items for database

--- app/src/main/java/com/openclassrooms/realestatemanager/estate_manager/RealEstateManagerActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/estate_manager/RealEstateManagerActivity.kt
@@ -1,27 +1,24 @@
 package com.openclassrooms.realestatemanager.estate_manager
 
-import com.openclassrooms.realestatemanager.database.dao.ImagesDao
+import com.openclassrooms.realestatemanager.database.dao.ImageDao
 import com.openclassrooms.realestatemanager.database.dao.RealEstateDao
 import com.openclassrooms.realestatemanager.database.dao.SellerNameDao
-import com.openclassrooms.realestatemanager.database.tables.Images
-import com.openclassrooms.realestatemanager.database.tables.RealEstate
-import com.openclassrooms.realestatemanager.database.tables.SellerName
+import com.openclassrooms.realestatemanager.models.RealEstateManagerModel
+
 
 class RealEstateManagerActivity {
-    suspend fun initializeDatabase(realEstateDao: RealEstateDao, imageDao: ImagesDao, sellerNameDao: SellerNameDao) {
+    suspend fun initializeDatabase(realEstateDao: RealEstateDao, imageDao: ImageDao, sellerNameDao: SellerNameDao) {
         if (realEstateDao.getRowCount() == 0) {
-            // Générer quelques noms de vendeurs et les insérer
-            val sellerId1 = sellerNameDao.insert(SellerName("Vendeur 1"))
-            val sellerId2 = sellerNameDao.insert(SellerName("Vendeur 2"))
-
-            // Générer quelques biens immobiliers et les insérer
-            val realEstateId1 = realEstateDao.insert(RealEstate())
-            val realEstateId2 = realEstateDao.insert(RealEstate())
-
-            // Générer quelques images pour chaque bien immobilier
-            imageDao.insert(Image("url1", realEstateId1))
-            imageDao.insert(Image("url2", realEstateId1))
-            imageDao.insert(Image("url3", realEstateId2))
+            RealEstateManagerModel.getDefaultSellers().forEach { seller ->
+                sellerNameDao.insert(seller)
+            }
+            RealEstateManagerModel.getDefaultRealEstates().forEach { estate ->
+                realEstateDao.insert(estate)
+            }
+            RealEstateManagerModel.getDefaultImages().forEach { image ->
+                imageDao.insert(image)
+            }
         }
     }
+
 }
