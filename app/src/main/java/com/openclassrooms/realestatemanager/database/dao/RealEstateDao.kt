21665b6301c73b1c81db62a5f5a07759a949e113
Lucasdev <potronlucas9@outlook.fr>
2023-10-07 13:35:55 +0200

    Fix

--- app/src/main/java/com/openclassrooms/realestatemanager/database/dao/RealEstateDao.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/RealEstateDao.kt
@@ -1,24 +1,35 @@
 package com.openclassrooms.realestatemanager.database.dao
 
-import android.media.Image
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.openclassrooms.realestatemanager.database.tables.RealEstate
 import kotlinx.coroutines.flow.Flow
 
 
-@Dao
+    @Dao
     interface RealEstateDao {
         @Query("SELECT * FROM real_estate")
         fun getAll(): Flow<List<RealEstate>>
+
+        @Query("SELECT * FROM real_estate WHERE id = :id LIMIT 1")
+        fun getOneItem(id : Long): RealEstate?
+
+        @Query("SELECT * FROM real_estate WHERE name = :name")
+        fun getRealEstateByName(name: String): RealEstate
 
         @Insert(onConflict = OnConflictStrategy.IGNORE)
          fun insert(realEstate: RealEstate): Long
 
         @Query("SELECT COUNT(*) FROM real_estate")
          fun getRowCount(): Int
+
+        @Update
+        fun update(realEstate: RealEstate): Unit
+
+
     }
 
 
