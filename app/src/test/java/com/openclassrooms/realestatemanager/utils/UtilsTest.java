19441c5c7bc4eef42f6a1f5ce3ec9806e8b597e6
Florent Champigny <florent@bere.al>
2023-12-09 11:17:57 +0100

    added unit test on add new estate

--- app/src/test/java/com/openclassrooms/realestatemanager/utils/UtilsTest.java
+++ app/src/test/java/com/openclassrooms/realestatemanager/utils/UtilsTest.java
@@ -14,9 +14,25 @@ public class UtilsTest {
     }
 
     @Test
-    public void convertDollarToEuro_1() {
+    public void convertEuroToDollar_1002() {
+        int euro = 1002;
+        float result = Utils.convertEuroToDollar(euro);
+        assert result == 1234;
+    }
+
+    @Test
+    public void convertDollarToEuro_100() {
         int dollar = 100;
         float result = Utils.convertDollarToEuro(dollar);
         assert result == 81.0;
+    }
+
+
+
+    @Test
+    public void convertEuroToDollar_81() {
+        int euro = 81;
+        float result = Utils.convertEuroToDollar(euro);
+        assert result == 100;
     }
 }
